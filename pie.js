1b1822e6cdd8f8f0582eeda05205c0d6d0afb90c
michaeldy28 <102933151+michaeldy28@users.noreply.github.com>
2023-07-30 21:28:02 +0700

    Update pie.js

--- pie.js
+++ pie.js
@@ -20,6 +20,10 @@ function main() {
 
     d3.dsv(',', 'HallOfFame.csv').then(function(data) {
 
+        var filteredData = data.filter(function (d) {
+            return +d.Inducted >= 0; // Filter data for overall >= 89
+        });
+        
         // Create nationalityCounts as an object instead of an array
         var ASGCounts = {};
         filteredData.forEach(function(player) {
