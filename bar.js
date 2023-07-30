e3aa3cbbf7b63d886f5bddc365700570634b5b00
michaeldy28 <102933151+michaeldy28@users.noreply.github.com>
2023-07-30 20:26:20 +0700

    Update bar.js

--- bar.js
+++ bar.js
@@ -1,6 +1,4 @@
-<script>
 
-// set the dimensions and margins of the graph
 var margin = {top: 10, right: 30, bottom: 30, left: 40},
     width = 460 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
