0eea6e81c735ef007ac2053282a69d95465de2ff
michaeldy28 <102933151+michaeldy28@users.noreply.github.com>
2023-07-31 11:28:56 +0700

    Update pie.js

--- pie.js
+++ pie.js
@@ -66,8 +66,8 @@ function main() {
 
                 var tooltipText = tooltip.append('text')
                                         .attr('class', 'tooltip-text')
-                                        .attr('x', 30) // Adjust the x position of the text
-                                        .attr('y', height / 2) // Adjust the y position of the text
+                                        .attr('x', 300) // Adjust the x position of the text
+                                        .attr('y', height) // Adjust the y position of the text
                                         .text(d.data.ASG + " All Star Games Played: " + d.data.count);
 
 
