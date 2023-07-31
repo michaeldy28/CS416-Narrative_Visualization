b649c46d1976f109926e2734feb40fd57e3b36ec
michaeldy28 <102933151+michaeldy28@users.noreply.github.com>
2023-07-31 11:03:01 +0700

    Update pie.js

--- pie.js
+++ pie.js
@@ -111,15 +111,19 @@ function main() {
         
         // Add an unrelated annotation
         svg.append("text")
-            .attr("x", 5) // Adjust the x position of the unrelated text
+            .attr("x", 300) // Adjust the x position of the unrelated text
             .attr("y", 30) // Adjust the y position of the unrelated text
-            .text("Hover over each arc of the pie chart to see")
+            .text("You can put your mouse over a section to inspect them closer")
             .style("font-size", "17px")
             .style("fill", "black")
             .append("tspan") // Add a <tspan> element for the next line
             .attr("x", 5) // Adjust the x position of the second line
             .attr("dy", "1.2em") // Adjust the vertical offset to create space between lines
-            .text("the names of the players that represent that nationality.");
+            .text("this section is particularly large due to the All Star game");
+                    .append("tspan") // Add a <tspan> element for the next line
+            .attr("x", 5) // Adjust the x position of the second line
+            .attr("dy", "1.2em") // Adjust the vertical offset to create space between lines
+            .text("only being introduced in 1933 and houses many late 1800s and early 1900s players");
 
     });
 };
